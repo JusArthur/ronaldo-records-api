86184716d4833985b035820bfa96e7a950dc4a04
Qinglan <justinxmay@gmail.com>
2025-11-04 11:56:43 -0600

    Add match controller and response model for API handling

---
+++ src/api/v1/controllers/matchController.ts
@@ -0,0 +1,4 @@
+// External imports
+import { Request, Response, NextFunction } from "express";
+import { successResponse } from "../models/responseModel";
+import { HTTP_STATUS } from "../../constants/httpConstants";
